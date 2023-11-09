c30ddcf1847690a9925e3d79df2cf596e7f1741f
Preston <prhunguy@ucsc.edu>
2023-11-08 20:38:38 -0800

    adding rest of files

--- asgn3/queue.c
+++ asgn3/queue.c
@@ -5,7 +5,99 @@
 
 #include "queue.h"
 
-int main() {
+// Define the queue structure
+struct queue {
+    int size;                 // Maximum size of the queue
+    int front;                // Index for the front of the queue
+    int end;                  // Index for the end of the queue
+    void **data;              // Data array to store elements
+    pthread_mutex_t mutex;    // Mutex for thread safety
+    pthread_cond_t not_empty; // Condition variable for non-empty queue
+    pthread_cond_t not_full;  // Condition variable for non-full queue
+};
 
-    return 0;
+// Create a new queue with the specified size
+queue_t *queue_new(int size) {
+    queue_t *q = (queue_t *)malloc(sizeof(queue_t)); // Allocate memory for the queue
+    if (q == NULL) {
+        return NULL;
+    }
+
+    q->size = size; // Set the maximum size of the queue
+    q->front = 0;  // Initialize the front index
+    q->end = 0;   // Initialize the last index
+
+    // Allocate memory for the data array to store elements
+    q->data = (void **)malloc(sizeof(void *) * size);
+    if (q->data == NULL) {
+        free(q);
+        return NULL;
+    }
+
+    // Initialize a mutex for thread safety
+    pthread_mutex_init(&q->mutex, NULL);
+
+    // Initialize condition variables for non-empty and non-full queues
+    pthread_cond_init(&q->not_empty, NULL);
+    pthread_cond_init(&q->not_full, NULL);
+
+    return q; // Return the initialized queue
+}
+
+// Delete the queue and free allocated memory
+void queue_delete(queue_t **q) {
+    if (q == NULL || *q == NULL) {
+        return;
+    }
+
+    queue_t *queue = *q; // Get the pointer to the queue
+    free(queue->data);   // Free the memory used by the data array
+
+    // Destroy the mutex and condition variables
+    pthread_mutex_destroy(&queue->mutex);
+    pthread_cond_destroy(&queue->not_empty);
+    pthread_cond_destroy(&queue->not_full);
+
+    free(queue); // Free the memory used by the queue
+    *q = NULL;   // Set the pointer to the queue to NULL
+}
+
+// Push an element onto the queue
+bool queue_push(queue_t *q, void *elem) {
+    pthread_mutex_lock(&q->mutex); // Lock the mutex for thread safety
+
+    // Wait until the queue is not full
+    while ((q->end + 1) % q->size == q->front) {
+        pthread_cond_wait(&q->not_full, &q->mutex);
+    }
+
+    // Store the element in the data array and update the last index
+    q->data[q->end] = elem;
+    q->end = (q->end + 1) % q->size;
+
+    // Signal that the queue is not empty and unlock the mutex
+    pthread_cond_signal(&q->not_empty);
+    pthread_mutex_unlock(&q->mutex);
+
+    return true; // Return true to indicate a successful push
+}
+
+// Pop an element from the queue
+bool queue_pop(queue_t *q, void **elem) {
+    pthread_mutex_lock(&q->mutex); // Lock the mutex for thread safety
+
+    // Wait until the queue is not empty
+    while (q->front == q->end) {
+        pthread_cond_wait(&q->not_empty, &q->mutex);
+    }
+
+    // Retrieve the element from the data array and update the front index
+    *elem = q->data[q->front];
+    q->front = (q->front + 1) % q->size;
+
+    // Signal that the queue is not full and unlock the mutex
+    pthread_cond_signal(&q->not_full);
+    pthread_mutex_unlock(&q->mutex);
+
+    return true; // Return true to indicate a successful pop
 }
--- asgn3/rwlock.c
+++ asgn3/rwlock.c
@@ -5,7 +5,104 @@
 
 #include "rwlock.h"
 
-int main() {
+// Define the structure for the reader-writer lock
+typedef struct rwlock {
+    int readers;                // Number of current readers
+    int writers;                // Number of current writers
+    int n;                      // N-WAY parameter
+    PRIORITY priority;          // Reader-writer lock priority
+    pthread_mutex_t mutex;      // Mutex for thread safety
+    pthread_cond_t readers_cond; // Condition variable for readers
+    pthread_cond_t writers_cond; // Condition variable for writers
+} rwlock_t;
 
-    return 0;
+// Create a new reader-writer lock
+rwlock_t *rwlock_new(PRIORITY p, uint32_t n) {
+    rwlock_t *rwlock = (rwlock_t *)malloc(sizeof(rwlock_t)); // Allocate memory for the reader-writer lock
+    if (rwlock == NULL) {
+        return NULL;
+    }
+
+    rwlock->readers = 0; // Initialize the number of readers to 0
+    rwlock->writers = 0; // Initialize the number of writers to 0
+    rwlock->n = n; // Set the N-WAY parameter
+    rwlock->priority = p; // Set the reader-writer lock priority
+
+    // Initialize the mutex and condition variables for readers and writers
+    pthread_mutex_init(&rwlock->mutex, NULL);
+    pthread_cond_init(&rwlock->readers_cond, NULL);
+    pthread_cond_init(&rwlock->writers_cond, NULL);
+
+    return rwlock; // Return the initialized reader-writer lock
 }
+
+// Delete the reader-writer lock and free allocated memory
+void rwlock_delete(rwlock_t **l) {
+    if (l == NULL || *l == NULL) {
+        return;
+    }
+
+    rwlock_t *rwlock = *l;
+
+    // Destroy the mutex and condition variables
+    pthread_mutex_destroy(&rwlock->mutex);
+    pthread_cond_destroy(&rwlock->readers_cond);
+    pthread_cond_destroy(&rwlock->writers_cond);
+
+    free(rwlock); // Free the memory used by the reader-writer lock
+    *l = NULL; // Set the pointer to the reader-writer lock to NULL
+}
+
+// Acquire the reader lock
+void reader_lock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->mutex);
+
+    // Wait while there are writers or the priority is set to WRITERS and there are readers
+    while ((rw->writers > 0) || (rw->priority == WRITERS && rw->readers > 0)) {
+        pthread_cond_wait(&rw->readers_cond, &rw->mutex);
+    }
+
+    rw->readers++; // Increment the number of readers
+    pthread_mutex_unlock(&rw->mutex);
+}
+
+// Release the reader lock
+void reader_unlock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->mutex);
+    rw->readers--; // Decrement the number of readers
+
+    if (rw->readers == 0) {
+        pthread_cond_signal(&rw->writers_cond); // Signal a waiting writer if there are no more readers
+    }
+
+    pthread_mutex_unlock(&rw->mutex);
+}
+
+// Acquire the writer lock
+void writer_lock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->mutex);
+
+    // Wait while there are readers or writers
+    while ((rw->readers > 0) || (rw->writers > 0)) {
+        pthread_cond_wait(&rw->writers_cond, &rw->mutex);
+    }
+
+    rw->writers++; // Increment the number of writers
+    pthread_mutex_unlock(&rw->mutex);
+}
+
+// Release the writer lock
+void writer_unlock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->mutex);
+    rw->writers--; // Decrement the number of writers
+
+    if (rw->priority == N_WAY && rw->writers == 0) {
+        pthread_cond_broadcast(&rw->readers_cond); // Broadcast to waiting readers for N-WAY priority
+    } else {
+        pthread_cond_signal(&rw->writers_cond); // Signal a waiting writer
+    }
+
+    pthread_mutex_unlock(&rw->mutex);
+}
+
+
