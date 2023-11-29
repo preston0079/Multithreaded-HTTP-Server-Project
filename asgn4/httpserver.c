ec8590fdb6030a9e319506159f449240fbbf0415
Preston <prhunguy@ucsc.edu>
2023-11-28 19:22:17 -0800

    1 success

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -1,165 +1,361 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <pthread.h>
-// #include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-// #include <err.h>
-// #include <errno.h>
-// #include <fcntl.h>
-// #include <signal.h>
-// #include <unistd.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
-// #include "asgn2_helper_funcs.h"
-// #include "connection.h"
-// #include "queue.h"
-// #include "response.h"
-// #include "request.h"
-// #include "rwlock.h"
+#include "asgn2_helper_funcs.h"
+#include "connection.h"
+#include "debug.h"
+#include "queue.h"
+#include "response.h"
+#include "request.h"
+#include "rwlock.h"
 
-// typedef struct rwlockHTNodeObj {
-//     rwlock_t *rwlock;
-//     rwlockHTNode *next;
-// } rwlockHTNodeObj;
+// Work used from previous class - hash table functions
 
-// typedef struct rwlockHTObj {
+typedef struct rwlockHTNodeObj {
+    char *uri;
+    rwlock_t *rwlock;
+    struct rwlockHTNodeObj *next;
+} rwlockHTNodeObj;
 
-// } rwlockHTObj;
+typedef rwlockHTNodeObj* rwlockHTNode;
 
-// typedef rwlockHTObj *rwlockHT;
+typedef struct rwlockHTObj {
+    rwlock_t **rwlocks;
+} rwlockHTObj;
 
-// typedef struct ThreadObj {
-//     pthread_t thread;
-//     int id;
-//     rwlockHT_t *rwlockHT_t;
-// } ThreadObj;
+typedef rwlockHTObj *rwlockHT;
 
-// typedef ThreadObj *Thread;
+typedef struct Table {
+    struct rwlockHTNodeObj **buckets;
+    size_t num_buckets;
+} Table;
 
-// void handle_connection(int);
+typedef struct ThreadObj {
+    pthread_t thread;
+    int id;
+    // rwlockHT *rwlockHT;
+    Table *rwlockHT;
+    queue_t *queue;
+} ThreadObj;
 
-// void handle_get(conn_t *);
-// void handle_put(conn_t *);
-// void handle_unsupported(conn_t *);
+typedef ThreadObj *Thread;
 
-// int main(int argc, char **argv) {
+queue_t *queue;
+// rwlockHT *rwlock_table;
 
-//     if (argc < 2) {
-//         warnx("wrong arguments: %s port_num", argv[0]);
-//         fprintf(stderr, "usage: %s <port>\n", argv[0]);
-//         return EXIT_FAILURE;
-//     }
+Table *hashtable;
 
-//     // Use getopt to get the number of threads
-//     int t; //thread count (default 4)
+pthread_mutex_t file_lock;
+pthread_mutex_t table_mutex;
+pthread_mutex_t audit_lock;
 
-//     char *endptr = NULL;
-//     size_t port = (size_t) strtoull(argv[1], &endptr, 10);
-//     if (endptr && *endptr != '\0') {
-//         warnx("invalid port number: %s", argv[1]);
-//         return EXIT_FAILURE;
-//     }
+Table *create_table(size_t num_buckets) {
+	Table* t = calloc(1,sizeof(Table)); //allocate space for the table
+	t->buckets = calloc(num_buckets, sizeof(rwlockHTNodeObj*)); //allocate space for the array of buckets
+	t->num_buckets = num_buckets; //set the number of buckets/size of the hashtable
+	return t;
+}
 
-//     signal(SIGPIPE, SIG_IGN);
-//     Listener_Socket sock;
-//     listener_init(&sock, port);
+unsigned long hash(char *str) {
+    unsigned long hash = 5381;
+    int c;
 
-//     Thread threads[t];
-//     rwlockHT *rwlock_ht = new_rwlockHT(t);
+    while (*str != '\0') {
+        c = *str;
+        hash = ((hash << 5) + hash) + (unsigned char)c; // hash * 33 + c
+        str++;
+    }
+    return hash;
+}
 
-//     // pthread_t *tids = malloc(sizeof(pthread_t * t));
+rwlockHTNodeObj *add_node_to_list(char *URI, rwlock_t *rwlock, rwlockHTNodeObj *bucket) {
+	rwlockHTNodeObj* new_node;
+	new_node = calloc(1, sizeof(rwlockHTNodeObj));
+	new_node->uri = strdup(URI);
+	new_node->rwlock = rwlock;
+	new_node->next = bucket; //attach node to rest of linked list
+	return new_node;
+}
 
-//     // Create worker threads
-//     for (int i = 0; i <= t; ++i) {
-//         threads[i] = malloc(sizeof(ThreadObj));
-//         threads[i]->if = i;
-//         threads[i]->rwlockHT = rwlock_ht;
+void add_to_table(char *URI, rwlock_t *rwlock, Table *table) { //adds a customer to a table; code adapted from demo
+	unsigned long hashvalue = hash(URI);
+	size_t index = hashvalue % table->num_buckets;
+	rwlockHTNodeObj *linkedlist = (table->buckets)[index];
+	//need to consider if customer email is already in there
+	while(linkedlist && strcmp((linkedlist->uri),URI)!= 0)
+	{
+		linkedlist = linkedlist->next;
+	}
+	//we found a linked list node that matches our email that we had previously hashed
+	if(linkedlist != NULL) 
+	{
+		//if email is already in there, we put a new entry containing the newly entered attributes
+        //will have to modify after thinking some more
+		free(linkedlist->rwlock);
+		linkedlist->rwlock = rwlock;
+	}
+	//else, we have a new customer that we need to add to our table and bucket
+	else
+	{
+		(table->buckets)[index] = add_node_to_list(URI, rwlock, (table->buckets)[index]); //puts a new node into that bucket
+	}
+	return;	
+}
 
-//         pthread_create(tids + 1, NULL, (void *(*) (void *) ) handle_connection, NULL);
-//     }
+rwlockHTNodeObj* find_URI(char* URI, Table* table) {
+	size_t index = hash(URI) % (table->num_buckets);
+	rwlockHTNodeObj *linkedlist = (table->buckets)[index];
+	while(linkedlist != NULL)
+	{
+		if(strcmp(linkedlist->uri, URI) == 0)
+		{
+			return linkedlist;
+		}
+		linkedlist = linkedlist->next;
+	}
+	return NULL; //user was not found
+}
 
-//     while (1) {
-//         uintptr_t connfd = listener_accept(&sock);
-//         queue_push(queue, (void *) connfd);
-//     }
+void handle_connection(int);
 
-//     return EXIT_SUCCESS;
-// }
+void worker_thread(void *args);
 
-// void worker_thread(void) {
+void handle_get(conn_t *);
+void handle_put(conn_t *);
+void handle_unsupported(conn_t *);
 
-//     Thread thread = (Thread) args;
-//     int id = thread->id;
-//     rwlockHT *rwlock_ht = thread->rwlockHT;
+int main(int argc, char **argv) {
 
-//     while (1) {
-//         uintptr_t connfd = 0;
-//         queue_pop(queue, (void **) &connfd);
-//         handle_connection(connfd);
-//         close(connfd);
-//     }
-// }
+    if (argc < 2) {
+        warnx("wrong arguments: %s port_num", argv[0]);
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-// void handle_connection(int connfd) {
+    // Use getopt to get the number of threads
+    size_t t = 4; //thread count (default 4)
+    int opt;
 
-//     conn_t *conn = conn_new(connfd);
+    // Parse command-line options using getopt (used previous class getopt format)
+    while ((opt = getopt(argc, argv, "t:")) != -1) {
+        switch (opt) {
+            case 't':
+                t = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s -t <number_of_threads> <port>\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
-//     const Response_t *res = conn_parse(conn);
+    char *endptr = NULL;
+    size_t port = (size_t) strtoull(argv[1], &endptr, 10);
+    if (endptr && *endptr != '\0') {
+        warnx("invalid port number: %s", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-//     if (res != NULL) {
-//         conn_send_response(conn, res);
-//     } else {
-//         debug("%s", conn_str(conn));
-//         const Request_t *req = conn_get_request(conn);
-//         if (req == &REQUEST_GET) {
-//             handle_get(conn);
-//         } else if (req == &REQUEST_PUT) {
-//             handle_put(conn);
-//         } else {
-//             handle_unsupported(conn);
-//         }
-//     }
+    signal(SIGPIPE, SIG_IGN);
+    Listener_Socket sock;
+    listener_init(&sock, port);
 
-//     // while(1){
+    // Thread threads[t];
+    ThreadObj* threads[t];
+    Table *rwlock_ht = create_table(100);
+    queue_t *queue = queue_new(t);
+    pthread_t *thread_ids = malloc(sizeof(pthread_t) * t);
 
-//     //     uintptr_t connfd = 0;
-//     //     queue_pop(queue, (void **) &connfd);
 
-//     //     conn_t * conn = conn_new(connfd);
+    // Create worker threads
+    for (size_t i = 0; i <= t; ++i) {
+        threads[i] = malloc(sizeof(ThreadObj));
+        threads[i]->id = i;
+        threads[i]->rwlockHT = rwlock_ht;
+        threads[i]->queue = queue;
 
-//     //     const Response_t *res = conn_parse(conn);
+        pthread_create(&thread_ids[i], NULL, (void *(*) (void *)) worker_thread, (void *) threads[i]);
+    }
 
-//     //     if (res != NULL) {
-//     //         conn_send_resposne(conn, res);
-//     //     } else {
-//     //         debug("%s", conn_str(conn));
-//     //         const Request_t *req = conn_get_request(conn);
-//     //         if (req == &REQUEST_GET) {
-//     //             handle_get(conn);
-//     //         } else if (req == &REQUEST_PUT) {
-//     //             handle_put(conn);
-//     //         } else {
-//     //             handle_unsupported(conn);
-//     //         }
-//     //     }
+    while (1) {
+        uintptr_t connfd = listener_accept(&sock);
+        queue_push(queue, (void *) connfd);
+    }
 
-//     //     conn_delete(&conn);
-//     // }
-// }
+    return EXIT_SUCCESS;
+}
 
-// void handle_get(conn_t *conn) {
-// }
 
-// void handle_push(conn_t *conn) {
-// }
+void worker_thread(void *args) {
 
-// void handle_unsupported(conn_t *conn) {
-//     debug("handling unsupported request");
+    Thread thread = (Thread) args;
+    //int id = thread->id;
+    //rwlockHT *rwlock_ht = thread->rwlockHT;
+    queue_t *queue = thread->queue;
 
-//     // send responses
-//     conn_send_response(conn, &RESPONSE_NOT_IMPLEMENTED);
-// }
+    while (1) {
+        uintptr_t connfd = 0;
+        queue_pop(queue, (void **) &connfd);
+        handle_connection(connfd);
+        //close(connfd);
+    }
+}
 
-int main() {
-    return 0;
+void handle_connection(int connfd) {
+
+    conn_t *conn = conn_new(connfd);
+
+    const Response_t *res = conn_parse(conn);
+
+    if (res != NULL) {
+        conn_send_response(conn, res);
+    } else {
+        debug("%s", conn_str(conn));
+        const Request_t *req = conn_get_request(conn);
+        if (req == &REQUEST_GET) {
+            handle_get(conn);
+        } else if (req == &REQUEST_PUT) {
+            handle_put(conn);
+        } else {
+            handle_unsupported(conn);
+        }
+    }
+}
+
+void handle_get(conn_t *conn) {
+
+    const Response_t *res = NULL;
+
+    char *uri = conn_get_uri(conn);
+    debug("GET request no implemented. But we want to get %s", uri);
+    bool existed = access(uri, F_OK) == 0;
+
+    pthread_mutex_lock(&file_lock);
+
+    // 1. Open the file.
+    int fd = open(uri, O_RDONLY);
+    if (fd < 0) {
+        debug("%s: %d", uri, errno);
+
+        if (errno == EACCES) {
+            res = &RESPONSE_FORBIDDEN;
+        } else if (errno == ENOENT) {
+            res = &RESPONSE_NOT_FOUND;
+        } else {
+            res = &RESPONSE_INTERNAL_SERVER_ERROR;
+        }
+
+        goto out;
+    }
+
+    // get_rwlock---------------------------------
+    pthread_mutex_lock(&table_mutex);
+    rwlock_t *rwlock;
+    rwlockHTNodeObj* node;
+    node = find_URI(uri, hashtable);
+    if (node != NULL){
+        rwlock = node->rwlock;
+    } else {
+        rwlock = rwlock_new(N_WAY, 1);
+        add_to_table(uri, rwlock, hashtable);
+    }
+    pthread_mutex_unlock(&table_mutex);
+    // get_rwlock---------------------------------
+
+    reader_lock(rwlock);
+    pthread_mutex_unlock(&file_lock);
+
+    // 2. Get the size of the file.
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        res = &RESPONSE_INTERNAL_SERVER_ERROR;
+        goto out;
+    }
+
+    // 3. Check if the file is a directory.
+    if (S_ISDIR(st.st_mode)) {
+        res = &RESPONSE_FORBIDDEN; // Assuming directories are forbidden
+        goto out;
+    }
+
+    // 4. Send the file
+    if(res == NULL && existed){
+        res = conn_send_file(conn, fd, st.st_size);
+        res = &RESPONSE_OK;
+    }
+    
+    reader_unlock(rwlock);
+    close(fd);
+
+out:
+    // Send the response
+    conn_send_response(conn, res);
+}
+
+void handle_put(conn_t *conn) {
+    
+    const Response_t *res = NULL;
+
+    char *uri = conn_get_uri(conn);
+    debug("PUT request no implemented. But we want to get %s", uri);
+
+    // Check if file already exists before opening it.
+    bool existed = access(uri, F_OK) == 0;
+
+    debug("%s existed? %d", uri, existed);
+
+    // pthread_mutex_lock(&mutex);
+    // if(rwlock_ht_contains(rwlock_ht, uri)){
+    //     rwlock = rwlock_ht_get(rwlock_ht, uri);
+    // }else{
+    //     rwlock = rwlock_new[N_WAY, 1];
+    //     rwlock_ht_set(rwlock_ht, rwlock, uri);
+    // }
+    // pthread_mutex_unlock(&mutex);
+
+    // writer_lock(rwlock);
+
+    // Open the file.
+    int fd = open(uri, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+    if (fd < 0) {
+        debug("%s: %d", uri, errno);
+        if (errno == EACCES || errno == EISDIR || errno == ENOENT) {
+            res = &RESPONSE_FORBIDDEN;
+            goto out;
+        } else {
+            res = &RESPONSE_INTERNAL_SERVER_ERROR;
+            goto out;
+        }
+    }
+
+    res = conn_recv_file(conn, fd);
+
+    if (res == NULL && existed) {
+        res = &RESPONSE_OK;
+    } else if (res == NULL && !existed) {
+        res = &RESPONSE_CREATED;
+    }
+
+    //writer_unlock(rwlock);
+    close(fd);
+
+out:
+    conn_send_response(conn, res);
+
+}
+
+void handle_unsupported(conn_t *conn) {
+    debug("handling unsupported request");
+
+    // send responses
+    conn_send_response(conn, &RESPONSE_NOT_IMPLEMENTED);
 }
