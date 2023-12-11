465d408f68a8aad076672328863b7b737bc5aeb8
Preston <prhunguy@ucsc.edu>
2023-12-10 16:47:47 -0800

    pass2

--- asgn5/cacher.c
+++ asgn5/cacher.c
@@ -4,28 +4,21 @@
 
 // Help recieved from TA Mitchell pseudocode and help recieved from classmate - all help given on whiteboard no code was shared
 
-// Define the cache policies
-typedef enum { 
-    FIFO, 
-    LRU, 
-    Clock 
-} Policy;
+// enum policies
+typedef enum { FIFO, LRU, Clock } Policy;
 
-// Define a node in the cache
 typedef struct Node {
     char *data; // Was void instead of char previosly
     struct Node *next;
     int ref_bit; // Only used in Clock policy
 } Node;
 
-// Define a linked list
 typedef struct List {
     Node *head;
     int size;
     int max_size;
 } List;
 
-// Define the cache structure
 typedef struct Cache {
     Policy policy;
     List *list;
@@ -36,7 +29,6 @@ typedef struct Cache {
     int num_capacity_misses;
 } Cache;
 
-// Function prototypes
 int insert_fifo(Cache *cache, void *element);
 int insert_lru(Cache *cache, void *element);
 int insert_clock(Cache *cache, void *element);
@@ -51,7 +43,7 @@ void list_move_to_end(List *list, char *element);
 Node *list_get_element_at_index(List *list, int index);
 void list_overwrite_at_index(List *list, int index, char *data);
 int list_get_max_size(List *list);
-void list_free(List *list);
+void free_list(List *list);
 
 // void print_list(List *list) {
 //     if (list == NULL || list->head == NULL) {
@@ -67,7 +59,6 @@ void list_free(List *list);
 //     printf("\n");
 // }
 
-// Function to insert into FIFO cache
 int insert_fifo(Cache *cache, void *element) {
     if (cache == NULL || element == NULL) {
         return -1; // Error
@@ -115,7 +106,7 @@ int insert_lru(Cache *cache, void *element) {
     // Check if the element is in the cache
     if (list_contains_TF(cache->list, element) == 1) {
         list_move_to_end(cache->list, element);
-        
+
         return 1;
     }
 
@@ -201,7 +192,7 @@ int insert_clock(Cache *cache, void *element) {
 
     // if the list is not full - add the element to the back of the list (if it's a miss)
     list_add_back(cache->list, element);
-    
+
     //print_list(cache->list);
     return 0; // return false if the element was NOT in the cache
 }
@@ -213,42 +204,39 @@ void free_cache(Cache *cache) {
     }
 
     // Free the memory for the cache list
-    list_free(cache->list);
+    free_list(cache->list);
 
     // Free the memory for the history list
-    list_free(cache->historylist);
+    free_list(cache->historylist);
 
     // Free the memory for the cache itself
     free(cache);
 }
 
-// Function to check if the list is full
 int list_is_full(List *list) {
     return list->size == list->max_size;
 }
 
-// Function to add a new node with data to the back of the list
 void list_add_back(List *list, char *data) {
     if (list == NULL || data == NULL) {
-        return; // Error: Invalid input
+        return; // NULL error
     }
 
-    // Allocate memory for the new data by duplicating the provided string
-    char *new_data = strdup((char *)data);
+    // Duplicate the data
+    char *new_data = strdup(data);
     if (new_data == NULL) {
         perror("Failed to allocate memory for a new node");
-        exit(EXIT_FAILURE); // Exit the program in case of memory allocation failure
+        exit(EXIT_FAILURE);
     }
 
     // Allocate memory for the new node
-    Node *new_node = (Node *)malloc(sizeof(Node));
+    Node *new_node = (Node *) malloc(sizeof(Node));
     if (new_node == NULL) {
         perror("Failed to allocate memory for a new node");
-        free(new_data); // Free the previously allocated data in case of failure
-        exit(EXIT_FAILURE); // Exit the program in case of memory allocation failure
+        free(new_data); // Allocated new_data before so if there is failure then free this as well
+        exit(EXIT_FAILURE);
     }
 
-    // Initialize the new node with the provided data and set next to NULL
     new_node->data = new_data;
     new_node->next = NULL;
 
@@ -257,35 +245,36 @@ void list_add_back(List *list, char *data) {
         // If the list is empty, the new node becomes the head of the list
         list->head = new_node;
     } else {
-        // If the list is not empty, traverse to the end to add the new node
+        // If the list is not empty, go to the end to add the new node
         Node *current = list->head;
         while (current->next != NULL) {
             current = current->next;
         }
-        current->next = new_node; // Add the new node to the end of the list
+        current->next = new_node;
     }
 
-    list->size++; // Increment the size of the list
+    list->size++;
 }
 
-// Function to remove the front node of the list
 void list_remove_front(List *list) {
-    // Check for NULL pointers or an empty list
     if (list == NULL || list->head == NULL) {
-        return; // Error: Invalid input or an empty list
+        return;
     }
 
-    Node *front = list->head; // Get the front node of the list
-    list->head = front->next; // Update the head to the next node in the list
-    free(front); // Free the memory occupied by the removed front node
+    Node *front = list->head;
+    list->head = front->next;
 
-    list->size--; // Decrement the size of the list
+    // Free the data and the node
+    free(front->data);
+    free(front);
+
+    list->size--;
 }
 
-// Function to check if the list contains a specific element
+// Function to check if the list contains a specific element (returns 1 or 0)
 int list_contains_TF(List *list, char *element) {
     if (list == NULL || element == NULL) {
-        // return 0;
+        return -1;
     }
 
     Node *current = list->head;
@@ -298,10 +287,9 @@ int list_contains_TF(List *list, char *element) {
     }
 
     return 0; // Element not found in the list
-
 }
 
-// Function to search for an element in the list and return its corresponding node
+// Function to search for an element in the list and returns the node
 Node *list_search_for_element(List *list, char *element) {
     if (list == NULL || element == NULL) {
         return NULL;
@@ -310,15 +298,14 @@ Node *list_search_for_element(List *list, char *element) {
     Node *current = list->head;
     while (current != NULL) {
         if (strcmp(current->data, element) == 0) {
-            return current; // Return the node with the specified element
+            return current;
         }
         current = current->next;
     }
 
-    return NULL; // Element not found in the list
+    return NULL;
 }
 
-// Function to move a specific element to the end of the list
 void list_move_to_end(List *list, char *element) {
     if (list == NULL || element == NULL || list->head == NULL) {
         return;
@@ -327,7 +314,7 @@ void list_move_to_end(List *list, char *element) {
     Node *current = list->head;
     Node *prev = NULL;
 
-    // Find the node with the specified element
+    // Finds the node
     while (current != NULL && strcmp(current->data, element) != 0) {
         prev = current;
         current = current->next;
@@ -355,7 +342,6 @@ void list_move_to_end(List *list, char *element) {
     }
 }
 
-// Function to get the element at a specific index in the list
 Node *list_get_element_at_index(List *list, int index) {
     if (list == NULL || index < 0 || index > list->size) {
         return NULL;
@@ -364,45 +350,43 @@ Node *list_get_element_at_index(List *list, int index) {
     Node *current = list->head;
     int count = 0;
 
-    // Traverse the list to the specified index
+    // Traverse the list to the given index
     while (current != NULL && count < index) {
         current = current->next;
         count++;
     }
 
-    return current; // Return the node at the specified index
+    return current;
 }
 
-// Function to overwrite the data at a specific index in the list
 void list_overwrite_at_index(List *list, int index, char *data) {
     if (list == NULL || index < 0 || index >= list->size || data == NULL) {
-        // return -1;
+        return;
     }
 
     Node *current = list->head;
     int count = 0;
 
-    // Traverse the list to the specified index
+    // Traverse the list to the given index
     while (current != NULL && count < index) {
         current = current->next;
         count++;
     }
 
     if (current != NULL) {
-        // Free the existing data to avoid memory leaks
+        // Free the original data
         free(current->data);
 
         // Duplicate the new data and assign it to the node
         current->data = strdup(data);
 
         if (current->data == NULL) {
-            perror("Failed to duplicate data");
-            // Handle the error, possibly return an error code
+            perror("Failed to assign duplicate data");
+            return;
         }
     }
 }
 
-// Function to get the maximum size of the list
 int list_get_max_size(List *list) {
     if (list == NULL) {
         return -1;
@@ -411,8 +395,7 @@ int list_get_max_size(List *list) {
     return list->max_size;
 }
 
-// Function to free the memory used by the list
-void list_free(List *list) {
+void free_list(List *list) {
     if (list == NULL) {
         return;
     }
@@ -423,37 +406,39 @@ void list_free(List *list) {
     // Free each node in the list
     while (current != NULL) {
         next = current->next;
+        free(current->data);
         free(current);
         current = next;
     }
+
+    list->head = NULL;
 
     // Free the memory for the list itself
     free(list);
 }
 
-// Main function
 int main(int argc, char *argv[]) {
-    // Parse command line arguments
-
     Policy policy = FIFO; // Default is FIFO
 
+    // Parse command line arguments
     if (argc < 3 || argc > 4) {
         fprintf(stderr, "Usage: %s [-N size] <policy>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if(strcmp(argv[1], "-N") != 0){
+    if (strcmp(argv[1], "-N") != 0) {
         fprintf(stderr, "Cache size error\n");
         return -1;
     }
 
     int size = atoi(argv[2]);
 
-    if(size < 1){
+    if (size < 1) {
         fprintf(stderr, "Cache size error\n");
         return -1;
     }
 
+    // Assigns policy
     const char *policy_str = argv[3];
     if (strcmp(policy_str, "-F") == 0) {
         policy = FIFO;
@@ -473,6 +458,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Set initial values
     cache->policy = policy;
     cache->list = (List *) malloc(sizeof(List));
     cache->list->head = NULL;
@@ -482,32 +468,24 @@ int main(int argc, char *argv[]) {
     cache->historylist = (List *) malloc(sizeof(List));
     cache->historylist->head = NULL;
     cache->historylist->size = 0;
-    cache->historylist->max_size = 0; // History list has no size limit
+    cache->historylist->max_size = 0;
 
     cache->num_compulsory_misses = 0;
     cache->num_capacity_misses = 0;
     cache->clock_pointer = 0;
 
-    // Read items from stdin
     char buffer[4096];
     while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
         // Remove newline character from the end
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        // Insert the item into the cache
         int result = -1;
         switch (policy) {
-        case FIFO: 
-            result = insert_fifo(cache, buffer); 
-            break;
-        
-        case LRU: 
-            result = insert_lru(cache, buffer); 
-            break;
-            
-        case Clock: 
-            result = insert_clock(cache, buffer); 
-            break;
+        case FIFO: result = insert_fifo(cache, buffer); break;
+
+        case LRU: result = insert_lru(cache, buffer); break;
+
+        case Clock: result = insert_clock(cache, buffer); break;
 
         default:
             perror("Policy Error");
